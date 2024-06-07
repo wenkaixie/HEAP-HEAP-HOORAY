944fd9ec13b28ea613e5d0d690d6a7a38c22eb41
shengweikoh <shengweikoh.2023@scis.smu.edu.sg>
2024-06-07 11:15:32 +0800

    fixed navbar

--- driversclutch/src/app/page.jsx
+++ driversclutch/src/app/page.jsx
@@ -1,8 +1,11 @@
+import Navbar from "./components/navbar/navbar";
 
 export default function Page() {
   return (
-    <div>
-      Homepage
-    </div>
+    <main>
+      <div>
+        homepage
+      </div>
+    </main>
   );
 }
---
+++ driversclutch/src/app/screens/home/page.jsx
@@ -0,0 +1,11 @@
+import Navbar from "@/app/components/navbar/navbar";
+
+export default function Page() {
+  return (
+    <main>
+      <div>
+        <Navbar />
+      </div>
+    </main>
+  );
+}
