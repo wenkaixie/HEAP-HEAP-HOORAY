35ebef39877c90ee94df261a1ffbee1dd946063a
Xie Wenkai <xwkofficial@gmail.com>
2024-07-07 15:08:04 +0800

    routing based on role

--- driversclutch/frontend/src/app/firebase/firebase_config.js
+++ driversclutch/frontend/src/app/firebase/firebase_config.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
 	apiKey: "AIzaSyC7GxjUDhjnIVdoKJ3-tGPqATkXtLPMpi8",
@@ -12,7 +13,9 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const FirestoreDB = getFirestore(app);
 
-export const FirestoreDB = getFirestore(app);
+export { FirestoreDB, auth };
 
 export default app;
