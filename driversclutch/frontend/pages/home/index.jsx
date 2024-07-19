abd5d42176976965ecd3bb81c78472b776611fc0
Xie Wenkai <xwkofficial@gmail.com>
2024-07-19 19:32:05 +0800

    try vercel 2

--- driversclutch/frontend/pages/home/index.jsx
+++ driversclutch/frontend/pages/home/index.jsx
@@ -18,7 +18,7 @@ const Dashboard = () => {
       if (!userDocID) {
       throw new Error('User document ID not found in localStorage');
       }
-      const response = await axios.get(`http://localhost:8001/students/homepage/lessons/?studentID=${userDocID}`);
+      const response = await axios.get(`https://heap-heap-hooray-lc3lka4s0-wenkais-projects.vercel.app/students/homepage/lessons/?studentID=${userDocID}`);
       console.log('API Response:', response.data);
       setBookingsData(response.data);
   } catch (error) {
@@ -32,7 +32,7 @@ const Dashboard = () => {
         if (!userDocID) {
         throw new Error('User document ID not found in localStorage');
         }
-        const response = await axios.get(`http://localhost:8001/students/profile/?id=${userDocID}`);
+        const response = await axios.get(`https://heap-heap-hooray-lc3lka4s0-wenkais-projects.vercel.app/students/profile/?id=${userDocID}`);
         console.log('API Response:', response.data);
         setProfileData(response.data.data);
     } catch (error) {
