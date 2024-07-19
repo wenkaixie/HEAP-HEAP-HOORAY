2d07855cd0f6e131777f6ee2fee053377f80dec9
Owen Goh <owengoh2001@gmail.com>
2024-07-19 15:45:41 +0800

    fixed the dotenv bug

--- driversclutch/backend/firebase/firebase.js
+++ driversclutch/backend/firebase/firebase.js
@@ -14,13 +14,14 @@
 // module.exports = admin;
 
 // firebase.js
-const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json');
+require("dotenv").config();
+const admin = require("firebase-admin");
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://driversclutch.firebaseio.com',
-  storageBucket: 'gs://driversclutch.appspot.com'
+	credential: admin.credential.cert(serviceAccount),
+	databaseURL: "https://driversclutch.firebaseio.com",
+	storageBucket: "gs://driversclutch.appspot.com",
 });
 
 const db = admin.firestore();
