cb57a6d69df282e90815e5274537fdec0c64e387
Xie Wenkai <xwkofficial@gmail.com>
2024-07-21 02:29:04 +0800

    Update index.js

--- driversclutch/backend/index.js
+++ driversclutch/backend/index.js
@@ -5,6 +5,8 @@ const bodyParser = require("body-parser");
 const PORT = 8001;
 
 const { db } = require("./firebase/firebase.js");
+// const authMiddleware = require('./middlewares/authMiddleware');
+// const errorHandler = require('./middlewares/errorMiddleware');
 const logger = require("./middlewares/loggerMiddleware");
 
 const app = express();
@@ -14,79 +16,74 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(logger); // Logging middleware
 app.use(bodyParser.json());
-
 const allowedOrigins = [
-    "https://driversclutch.vercel.app",
-    "http://localhost:3000",
-    "https://heap-heap-hooray-8w28-git-main-wenkais-projects.vercel.app",
-    "https://heap-heap-hooray-8w28-q3zsj5v9u-wenkais-projects.vercel.app",
-];
-
-const corsOptions = {
-    origin: function (origin, callback) {
-        // Allow requests with no origin (like mobile apps, curl, postman)
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) === -1) {
-            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    },
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    credentials: true, // to allow cookies to be sent along with requests
-};
-
+	"https://driversclutch.vercel.app",
+	"http://localhost:3000",
+	"https://heap-heap-hooray-8w28-git-main-wenkais-projects.vercel.app",
+	"https://heap-heap-hooray-8w28-q3zsj5v9u-wenkais-projects.vercel.app",
+  ];
+  
+  const corsOptions = {
+	origin: allowedOrigins,
+	methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+	credentials: true, // to allow cookies to be sent along with requests
+  };
+  
 app.use(cors(corsOptions));
 
-// Enable pre-flight requests for all routes
-app.options('*', cors(corsOptions));
-
-// Server check
+//server check
 app.listen(PORT, (error) => {
-    if (!error)
-        console.log("Server is Successfully Running, and App is listening on port " + PORT);
-    else console.log("Error occurred, server can't start", error);
+	if (!error)
+		console.log(
+			"Server is Successfully Running, and App is listening on port " + PORT
+		);
+	else console.log("Error occurred, server can't start", error);
 });
 
-// Routes
+//routes
 app.get("/", (req, res) => {
-    res.status(200).json("Welcome, your app is working well");
+	res.status(200).json("Welcome, your app is working well");
 });
 
-// Authentication
+//authentication
 app.use("/auth", require("./routes/auth"));
 
-// For path /students/privateInstructors
-app.use("/students/privateInstructors", require("./routes/instructorListRouter.js"));
+// for path /students/privateInstructors
+app.use(
+	"/students/privateInstructors",
+	require("./routes/instructorListRouter.js")
+); //check naming with claire
 
-// For path /instructors/availability
-app.use("/instructors/availability", require("./routes/instructorAvailabilityRouter.js"));
+// for path /instructors/availability
+app.use(
+	"/instructors/availability",
+	require("./routes/instructorAvailabilityRouter.js")
+); //check naming with zuwei
 
-// For path /students/booking
+// for path /students/booking
 app.use("/students/booking", require("./routes/lessonBookingRouter.js"));
 
-// For path /students/homepage
+// for path /students/homepage
 app.use("/students/homepage", require("./routes/studentHomeRouter.js"));
 
-// For path /students/profile
+//for path /students/profile
 app.use("/students/profile", require("./routes/studentProfileRouter.js"));
 
-// For path /instructors/profile
+//for path /instructors/profile
 app.use("/instructors/profile", require("./routes/instructorProfileRouter.js"));
 
-// For path /instructors/homepage
+// for path /instructors/homepage
 app.use("/instructors/homepage", require("./routes/instructorHomeRouter.js"));
 
-// For path /instructors/studentList
+// for path /instructors/studentList
 app.use("/instructors/studentList", require("./routes/studentListRouter.js"));
 
-// For path /students/balance
+// for path /students/balance
 app.use("/students/balance", require("./routes/balanceRouter.js"));
 
-// For path /students/theoryTest
+// for path /students/theoryTest
 app.use("/students/theoryTest", require("./routes/theoryTestRouter.js"));
 
-// For web scraping
 app.use("/webscraping", require("./routes/bookingRouter.js"));
 
 module.exports = app;
