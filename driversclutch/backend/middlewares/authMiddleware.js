9ea0dd6cf133ea7b6fccf026b4d0aa901fa7d80c
yuanqiyq <yuanqi.yong.2023@scis.smu.edu.sg>
2024-07-20 18:41:19 +0800

    readme

--- driversclutch/backend/middlewares/authMiddleware.js
+++ driversclutch/backend/middlewares/authMiddleware.js
@@ -1,19 +1,3 @@
-// const { admin } = require('../firebase/firebase');
-
-// const authenticateUser = async (req, res, next) => {
-//   const token = req.header('Authorization').replace('Bearer ', '');
-//   try {
-//     const decodedToken = await admin.auth().verifyIdToken(token);
-//     req.user = decodedToken;
-//     next();
-//   } catch (error) {
-//     res.status(401).json({ message: 'Unauthorized' });
-//   }
-// };
-
-// module.exports = authenticateUser;
-
-// middlewares/authMiddleware.js
 const admin = require('../firebase/firebase');
 
 const authenticate = async (req, res, next) => {
