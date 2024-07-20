3f7ae5a85ab8142395cf5c62da45ce29c5449e9b
Xie Wenkai <xwkofficial@gmail.com>
2024-07-20 20:51:53 +0800

    backend url

--- driversclutch/frontend/src/app/firebase/firebase_config.js
+++ driversclutch/frontend/src/app/firebase/firebase_config.js
@@ -20,6 +20,6 @@ const FirestoreDB = getFirestore(app);
 
 export { FirestoreDB, auth };
 
-export const url= "https://heap-heap-hooray-lc3lka4s0-wenkais-projects.vercel.app";
+export const url= "https://heap-heap-hooray-8t5parwxa-wenkais-projects.vercel.app/";
 
 export default app;
